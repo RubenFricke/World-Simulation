a42153311de6fbaf32e9ff804e1b4b0b6d9243fc
Niek <niek@pop-os.localdomain>
2022-02-11 14:16:26 +0100

    added basic p5 stuff

--- animal.js
+++ animal.js
@@ -8,3 +8,4 @@ class Animal {
       this.hunger = hunger;
     }
 }
+
---
+++ index.js
@@ -0,0 +1,21 @@
+let animals = []
+
+function setup(){
+        createCanvas(800, 800);
+        strokeWeight(1);
+        background(0);
+        rectMode(CENTER);
+        stroke(255);
+        fill(0);
+}
+
+function draw(){
+    background(0)
+    fill(255)
+    textSize(32)
+    text("Hello World", 10, 50)
+}
+
+function populate(){
+
+}
