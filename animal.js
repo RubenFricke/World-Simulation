7a9847477ac91efa681677531561bd94900f3039
RubenFricke <rubenfricke@gmail.com>
2022-02-11 13:00:27 +0100

    Added Animal class

---
+++ animal.js
@@ -0,0 +1,10 @@
+class Animal {
+  constructor(x, y, xSpeed, ySpeed, hunger)
+  {
+      this.x = x;
+      this.y = y;
+      this.xSpeed = xSpeed;
+      this.ySpeed = ySpeed;
+      this.hunger = hunger;
+    }
+}
