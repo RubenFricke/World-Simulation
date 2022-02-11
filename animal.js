92b47496d2099f3436e8b65545ad1a9c278039ab
Niek <niek@pop-os.localdomain>
2022-02-11 14:47:22 +0100

    added animal movements

--- animal.js
+++ animal.js
@@ -7,5 +7,23 @@ class Animal {
       this.ySpeed = ySpeed;
       this.hunger = hunger;
     }
+
+    update(){
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+      this.hunger -= 0.01;
+      if (this.x > width || this.x < 0) {
+        this.xSpeed *= -1;
+      }
+      if (this.y > height || this.y < 0) {
+        this.ySpeed *= -1;
+      }
+    }
+
+    display(){
+      fill(100/255*this.hunger, 100/255*this.hunger, 100/255*this.hunger);
+      rect(this.x, this.y, 20, 20);
+    }
 }
+
 
--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 let animals = []
+const width = window.innerWidth;
+const height = window.innerHeight;
+
 
 function setup(){
         createCanvas(800, 800);
         strokeWeight(1);
         background(0);
-        rectMode(CENTER);
+        rectMode(CORNER);
         stroke(255);
         fill(0);
+        populate();
 }
 
 function draw(){
-    background(0)
-    fill(255)
-    textSize(32)
-    text("Hello World", 10, 50)
+    background(0);
+    fill(255);
+    textSize(32);
+    for(let i = 0; i < animals.length; i++){
+        animals[i].update();
+        animals[i].display();
+        console.log('updating animal: ' + i);
+    }
 }
 
 function populate(){
-
+    for(let i = 0; i < 50; i++){
+        let x = random(width);
+        let y = random(height);
+        animal = new Animal(x, y, random(-1, 1), random(-1, 1), random(100));
+        animals.push(animal);
+    }
 }
